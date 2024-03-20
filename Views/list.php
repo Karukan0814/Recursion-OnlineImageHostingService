7ddd509b0f9a4ed9944e448a9cc15816db4ab019
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-03-20 10:14:43 -0700

    画像カード　リンク追加

--- Views/list.php
+++ Views/list.php
@@ -2,7 +2,7 @@
 
 
 ?>
-<div class="container">
+<div class="container"  style="height: 80vh;">
 <div class="row">
         <?php if (is_null($images) || count($images) === 0): ?>
             <div class="col">
@@ -10,7 +10,9 @@
             </div>
         <?php else: ?>
             <?php foreach ($images as $image): ?>
+                <a href="/show?uid=<?= htmlspecialchars($image['uid']) ?>" class="text-decoration-none">
                 <div class="col-md-4">
+
                 <div class="card mb-4" style="aspect-ratio: 3 / 2;">
                 <img src="<?= '/img/' . htmlspecialchars($image['uid']) . rawurlencode($image['name']) ?>" class="card-img-top" alt="<?= htmlspecialchars($image['name']) ?>" style="object-fit: contain; height: 100%; width: 100%;">
                 <div class="card-body">
@@ -18,6 +20,7 @@
                     <p class="card-text">viewcount: <?= $image['viewCount'] ? htmlspecialchars($image['viewCount']) : "0" ?></p>
                 </div>
             </div>
+        </a>
                 </div>
             <?php endforeach; ?>
         <?php endif; ?>
