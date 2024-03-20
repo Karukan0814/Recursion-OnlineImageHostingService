e928a404d1e4c6732ecb9ee6dfa3052e6fad0383
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-03-20 00:15:34 -0700

    期限切れ画像自動削除機能

---
+++ delete-unusedFiles.php
@@ -0,0 +1,39 @@
+<?php
+
+spl_autoload_extensions(".php");
+spl_autoload_register();
+
+use Database\MySQLWrapper;
+use Helpers\DatabaseHelper;
+
+
+
+
+// テーブルからexpire_datetimeが過去になっているファイルを削除
+$deletedImages = DatabaseHelper::deleteAllExpiredImages();
+
+if (!is_null($deletedImages) && count($deletedImages) > 0) {
+
+    // 実際のファイルを削除する
+    $fileDirectory = __DIR__ . "/img"; // プロジェクトのルートディレクトリに対する相対パス
+
+
+    // $this->log('fileDirectory :' . $fileDirectory);
+    $deletedimgs = [];
+    foreach ($deletedImages as $image) {
+        // ファイルのパスを作成
+        $filePath = $fileDirectory . "/" . $image['uid'] . rawurlencode($image['name']);
+
+        // ファイルが存在する場合は削除
+        if (file_exists($filePath)) {
+
+            unlink($filePath);
+            $deletedimgs[] = $image['uid'] . rawurlencode($image['name']);
+        }
+    }
+}
+
+// ログファイル出力
+$logFile = __DIR__ . "/logs/logs.log";
+$message = "deleted images below :\n" . implode(',', $deletedimgs);;
+file_put_contents($logFile, $message, FILE_APPEND);
